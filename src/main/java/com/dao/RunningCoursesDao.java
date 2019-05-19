e0fa4302b3f5ecab46f8b5d255397743ff0692ee
yhfei <yhfei@zhengyunkeji.com>
2019-05-19 23:45:33 +0800

    change to other

--- src/main/java/com/dao/RunningCoursesDao.java
+++ src/main/java/com/dao/RunningCoursesDao.java
@@ -1,4 +1,14 @@
 package com.dao;
 
 public interface RunningCoursesDao {
+    /**
+     *  private int id;
+     *     private int userId;
+     *     private int coursesId;
+     *     private int totalNumber;
+     *     private int currentNumber;
+     */
+    boolean addStudent(int userId,int coursesId);
+    boolean updateCnumber(int userId,int coursesId);
+    String findInfo(int userId,int coursesId);
 }
--- src/main/java/com/pojo/RunningCourses.java
+++ src/main/java/com/pojo/RunningCourses.java
@@ -1,4 +1,59 @@
 package com.pojo;
 
 public class RunningCourses {
+    private int id;
+    private int userId;
+    private int coursesId;
+    private int totalNumber;
+    private int currentNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getCoursesId() {
+        return coursesId;
+    }
+
+    public void setCoursesId(int coursesId) {
+        this.coursesId = coursesId;
+    }
+
+    public int getTotalNumber() {
+        return totalNumber;
+    }
+
+    public void setTotalNumber(int totalNumber) {
+        this.totalNumber = totalNumber;
+    }
+
+    public int getCurrentNumber() {
+        return currentNumber;
+    }
+
+    public void setCurrentNumber(int currentNumber) {
+        this.currentNumber = currentNumber;
+    }
+
+    public int getAverageLength() {
+        return averageLength;
+    }
+
+    public void setAverageLength(int averageLength) {
+        this.averageLength = averageLength;
+    }
+
+    private int averageLength;
 }
